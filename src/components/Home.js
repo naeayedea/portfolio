1047b97f9c9406d38eb5db845500a3aa33858129
naeayedea <steven.muirhead1999@gmail.com>
2022-07-19 22:42:01 +0100

    added small WIP notice to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,17 @@ class Home extends Component {
         return (
             <Template title={<h1>Home</h1>} content ={
                 <>
-                    <p>Hello from home</p>
+                    <div>
+                        <h1>Steven Muirhead</h1>
+                        <p>This website serves as a record of my notable projects and work. The section 'My Work' contains in depth information on the things I have been involved in at university and personal projects</p>
+                        <p>Please do not hesitate to contact me for work opportunities or further information! </p>
+                    </div>
+                    <br/>
+                    <div>
+                        <h1>Please note this site is a WIP</h1>
+                        <p>The basics are implemented, I will be adding content in the coming weeks</p>
+                    </div>
+                    <br/>
                 </>
             } />
         )
