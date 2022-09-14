57348a4b1851d64ee05d4937a07635b833afee9a
naeayedea <steven.muirhead1999@gmail.com>
2022-09-14 19:15:29 +0100

    keyword change

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -3,7 +3,7 @@
  * External Link is an easy way to ensure that all external links have the same styling and open in a new tab rather than
  * redirecting from current
  */
-let ExternalLink = (props) => {
+const ExternalLink = (props) => {
     return (
         <a href={props.href} target={"_blank"} rel={"noreferrer"} className="externalLink">
             {props.content}
