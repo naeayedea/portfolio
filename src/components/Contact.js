eb8a28beb6bddeb60c40b8d2c50e94f68f59ec9b
naeayedea <steven.muirhead1999@gmail.com>
2022-10-25 21:57:04 +0100

    fixed the logo link display

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,8 +17,8 @@ const Contact = () => {
                         <Item title={"LinkedIn"} pos={position} content={<>
                             <p>My LinkedIn contains in depth information about my education, skills, modules, test-scores etc. It is also the best way of getting in contact with me  </p>
                             <br/>
-                            <p className={"logo-link"}>
-                                <ExternalLink href="https://www.linkedin.com/in/stevenmuirhead/" content={<><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn Logo"/> My Profile</>}/>
+                            <p >
+                                <ExternalLink href="https://www.linkedin.com/in/stevenmuirhead/" content={<div className={"logo-link"}><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn Logo"/><div className={"link-wrapper"}><p>My Profile</p></div></div>}/>
                             </p>
                         </>}/>
                         <Item title={"GitHub"} pos={position} content={<>
@@ -27,7 +27,7 @@ const Contact = () => {
                             </p>
                             <br/>
                             <p className={"logo-link"}>
-                                <ExternalLink href="https://github.com/naeayedea" content={<><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo"/> https://github.com/naeayedea</>}/>
+                                <ExternalLink href="https://github.com/naeayedea" content={<div className={"logo-link"}><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo"/><div className={"link-wrapper"}><p>https://github.com/naeayedea</p></div></div>}/>
                             </p>
                         </>}/>
                     </div>
