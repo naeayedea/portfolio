8a8798b55a23246cf974136d43aaa6423d4cc90d
naeayedea <steven.muirhead1999@gmail.com>
2022-11-01 21:28:00 +0000

    extracted the link into a separate component for re-usability

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,23 +17,27 @@ const Contact = () => {
                         <Item title={"LinkedIn"} pos={position} content={<>
                             <p>My LinkedIn contains in depth information about my education, skills, modules, test-scores etc. It is also the best way of getting in contact with me  </p>
                             <br/>
-                            <div>
-                                <ExternalLink href="https://www.linkedin.com/in/stevenmuirhead/" content={<div className={"logo-link"}><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn Logo"/><div className={"link-wrapper"}><p>My Profile</p></div></div>}/>
-                            </div>
+                            <ContactLink href={"https://www.linkedin.com/in/stevenmuirhead/"} image={"https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"} alt={"LinkedIn Logo"} content={<p>My Profile</p>} />
                         </>}/>
                         <Item title={"GitHub"} pos={position} content={<>
                             <p>
                                 Whilst a significant number of my projects are hosted on my universities GitLab instance, some projects, including the react code for this website can be found at:
                             </p>
                             <br/>
-                            <div className={"logo-link"}>
-                                <ExternalLink href="https://github.com/naeayedea" content={<div className={"logo-link"}><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo"/><div className={"link-wrapper"}><p>https://github.com/naeayedea</p></div></div>}/>
-                            </div>
+                            <ContactLink href={"https://github.com/naeayedea"} image={"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"} alt={"GitHub logo"} content={<p>https://github.com/naeayedea</p>} />
                         </>}/>
                     </div>
                 </>
             } />
         )
+}
+
+const ContactLink = (props) => {
+    return (
+        <div className={"logo-link"}>
+            <ExternalLink href={props.href} content={<div className={"logo-link"}><img src={props.image} alt={props.alt}/><div className={"link-wrapper"}>{props.content}</div></div>}/>
+        </div>
+    )
 }
 
 export default Contact
