6e38767dc6aabafaf454b67e1aab4dcf8feacfd9
naeayedea <steven.muirhead1999@gmail.com>
2022-11-02 00:10:46 +0000

    added home page content with some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Template from './Template'
 
-const position = "pos-center";
 const Home = () => {
     return (
         <Template title={"Home"} content ={
-            <>
-                <p>Hello! My name is</p>
-                <p id={"name-large"}>Steven Muirhead</p>
-                <p id={"job-title"}>Fourth Year Computer and Electronic Systems | University of Strathclyde</p>
-                <p>Welcome to my portfolio website, here you can find information about me and some of the projects I have been involved in.</p>
-            </>
+            <div id={"home-container"}>
+                <div id={"home-view"}>
+                    <p id={"welcome-text"}>Hello! My name is</p>
+                    <p id={"name-large"}>Steven Muirhead</p>
+                    <p id={"job-title"}>Computer and Electronic Systems | University of Strathclyde</p>
+                    <br/>
+                    <p id={"intro-text"}>Welcome to my portfolio website! here you can find information about me and some of the projects I have been involved in.</p>
+                </div>
+            </div>
         } />
     )
 }
