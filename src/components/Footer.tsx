cea1314d8d2b62a763540968d91b9959a03df2c1
naeayedea <steven.muirhead1999@gmail.com>
2025-02-02 15:32:26 +0000

    made more space available on smaller screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,15 @@ import { ExternalLink } from "./Link";
 
 export default function Footer() {
     return (
-        <footer className="bg-white dark:bg-gray-800 bg-opacity-90 text-gray-800 dark:text-white py-4 shadow-md z-10">
-            <div className="container mx-auto px-4 flex flex-col lg:flex-row justify-between items-center">
-                <p>&copy; {getCurrentYear()} {personalDetails.name}. All rights reserved.</p>
-                <nav className="flex space-x-4 mt-4 lg:mt-0">
-                    <ExternalLink href={personalDetails.github} aria-label="GitHub">
-                        <Github className="w-6 h-6" />
+        <footer className="bg-white dark:bg-gray-800 bg-opacity-90 text-gray-800 dark:text-white py-2 sm:py-3 xl:py-4 shadow-md z-10">
+            <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+                <p className={"text-center text-sm md:text-base"} >&copy; {getCurrentYear()} {personalDetails.name}. All rights reserved.</p>
+                <nav className="flex space-x-2 md:space-x-4 ">
+                    <ExternalLink href={personalDetails.github} aria-label="GitHub" className={"z-40"}>
+                        <Github className="w-4 h-4 xs:w-5 xs:h-5 md:w-6 md:h-6" />
                     </ExternalLink>
-                    <ExternalLink href={personalDetails.linkedin} aria-label="LinkedIn">
-                        <Linkedin className="w-6 h-6" />
+                    <ExternalLink href={personalDetails.linkedin} aria-label="LinkedIn" className={"z-40"}>
+                        <Linkedin className="w-4 h-4 xs:w-5 xs:h-5 md:w-6 md:h-6" />
                     </ExternalLink>
                 </nav>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,10 +17,10 @@ export default function Header() {
     };
 
     return (
-        <header className="bg-white dark:bg-gray-800 bg-opacity-90 text-gray-800 dark:text-white py-4 shadow-md z-50">
-            <div className="container mx-auto px-4 flex justify-between items-center">
+        <header className="bg-white dark:bg-gray-800 bg-opacity-90 text-gray-800 dark:text-white py-2 md:py-3 xl:py-4 shadow-md z-50">
+            <div className="container mx-auto px-2 xs:px-4 flex justify-between items-center">
                 <ImageAndTitle className={"z-50"}/>
-                <ThemeToggle size={isAbove ? 20 : 16} className={isAbove ? "p-2" : "p-1"}/>
+                <ThemeToggle size={isAbove ? 20 : 16} className={`z-50 ${isAbove ? "p-2" : "p-1"}`}/>
                 <button
                     className="md:hidden text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors z-50 flex-shrink"
                     onClick={toggleMenu}
@@ -28,7 +28,7 @@ export default function Header() {
                 >
                     {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
-                <nav className={`fixed top-0 left-0 right-0 bottom-0 bg-white dark:bg-gray-800 bg-opacity-90 z-40 transform transition-transform duration-300 ease-in-out md:static md:bg-transparent md:transform-none ${isMenuOpen ? 'translate-y-0' : '-translate-y-full md:translate-y-0'}`}>
+                <nav className={`fixed top-0 left-0 right-0 bottom-0 bg-white dark:bg-gray-800 z-40 transform transition-transform duration-300 ease-in-out md:static md:bg-transparent md:transform-none ${isMenuOpen ? 'translate-y-0' : '-translate-y-full md:translate-y-0'}`}>
                     <div className="container mx-auto px-4 py-4 md:py-0 max-h-screen h-screen flex flex-col md:flex-none md:h-auto">
                         <div className="flex justify-between items-center mb-8 md:hidden">
                             <ImageAndTitle className={"opacity-0"}/>
@@ -41,9 +41,6 @@ export default function Header() {
                         </ul>
                     </div>
                 </nav>
-                {isMenuOpen && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 z-30 md:hidden" onClick={toggleMenu}></div>
-                )}
             </div>
         </header>
     );
